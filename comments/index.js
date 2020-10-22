36b34ffabf5124189cd52d722755a921a79c2c39
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-10-22 16:00:26 +0200

    add query, moderation and comments services

--- comments/index.js
+++ comments/index.js
@@ -17,7 +17,7 @@ app.post("/posts/:id/comments", async (req, res) => {
   const comments = commentsByPostId[postId] || [];
   comments.push({ id: commentId, content, status: "pending" });
 
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "CommentCreated",
     data: {
       id: commentId,
@@ -46,7 +46,7 @@ app.post("/events", async (req, res) => {
     });
     comment.status = status;
 
-    await axios.post("http://localhost:4005/events", {
+    await axios.post("http://event-bus-srv:4005/events", {
       type: "CommentUpdated",
       data: {
         id,
--- query/index.js
+++ query/index.js
@@ -47,7 +47,7 @@ app.post("/events", (req, res) => {
 app.listen(4002, async () => {
   console.log("Listening on 4002");
 
-  const response = await axios.get("http://localhost:4005/events");
+  const response = await axios.get("http://event-bus-srv:4005/events");
   for (let event of response.data) {
     console.log("Processing event:", event.type);
 
